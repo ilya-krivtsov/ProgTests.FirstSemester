a1c9dd958c747b93fa43784f3b103336b5fcda06
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2024-12-18 14:27:37 +0300

    Final test - task 1

---
+++ test_final/task_1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "binaryToDecimal.h"
+
+int main() {
+    printf("input binary number: ");
+    char input[256] = { 0 };
+    if (scanf("%255s", input) != 1) {
+        printf("failed to read string\n");
+        return 1;
+    }
+
+    char *decimal = binaryToDecimal(input);
+    if (decimal == NULL) {
+        printf("failed to convert string\n");
+        return 1;
+    }
+
+    printf("number as decimal: %s\n", decimal);
+
+    free(decimal);
+}
